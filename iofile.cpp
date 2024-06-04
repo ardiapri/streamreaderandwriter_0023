da6a0b024dfd364d2722b87aee6e4bf9470dc9b0
ardiapri <ardiapri882@gmail.com>
2024-06-04 17:32:52 +0700

    mebuka file dalam mode membaca

--- iofile.cpp
+++ iofile.cpp
@@ -23,5 +23,30 @@ int main() {
         //menulis dan memasukan nilai dari 'baris' ke dalam file
         outfile << baris << endl;
     }
-    
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+    //menunjuk ke sebuah file
+    infile.open("contohfilr.text");
+
+    cout << endl << ".= membuka dan membaca file " << endl;
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			//dan tampilkan di sini
+			cout << baris << '\n';
+		}
+		//tutup file tersebut setelah selesai
+		infile.close();
+	}
+	//jika tidak di temukan file maka akan menampilkan ini
+	else cout << "Unable to open file";
+	return 0;
 }
+
+    
